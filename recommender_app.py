ba2f82f5e5d4d4e53e824a889637ddefbe640d26
sathyavat <150167813+sathyavat@users.noreply.github.com>
2024-12-09 13:11:39 -0600

    Display 10 movie recommendations returned by the myIBCF function.
    Currently doesn't use `myIBCF` but just randomly samples 10 movies from the full dataset to display.

--- recommender_app.py
+++ recommender_app.py
@@ -4,8 +4,13 @@ import streamlit as st, numpy as np, pandas as pd
 st.set_page_config(layout="wide")
 
 from IBCF import load_movies, parse_movie_img, myIBCF
+# TODO: Clean up this import if unused after the TODO for 
+# step 2 below is completed.
+from IBCF import load_top_ten_movies
 
 st.title("Movie Recommender System")
+
+st.header("Rate your favorite movies!")
 
 # Loads the all the movies and selects 100 of them to be rated by the users
 all_movies = load_movies()
@@ -22,3 +27,15 @@ for i in range(10):
 
 if st.button("Submit"):
     st.write("I am the next biggest star of Hollywood!!")
+
+st.header("Here are the top 10 movies for you to check out next")
+
+# TODO: Replace this with actual implementation of 
+# 2) Use the ratings provided by the user as input for your myIBCF function.
+top_10_movies = all_movies.sample(n=10)
+cols = st.columns(10)
+for i in range(10):
+    movie = top_10_movies.iloc[i]
+    cols[i].image(parse_movie_img(movie["movie_id"]), caption=movie["title"], use_container_width=True)
+
+
